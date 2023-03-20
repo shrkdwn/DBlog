553264ab3e26cb1def9cc5301e63f4bfb09ff2a6
dwn <dongweinan1108@gmail.com>
2023-03-20 15:29:19 +0800

    feat: nature

--- chuangwa/src/router/index.ts
+++ chuangwa/src/router/index.ts
@@ -9,6 +9,16 @@ const routes: Array<RouteRecordRaw> = [
         path:'/',
         name:'home',
         component:() => import('../views/home/index.vue')
+      },
+      {
+        path:'/info',
+        name:'info',
+        component:() => import('../views/info/index.vue')
+      },
+      {
+        path:'/nature',
+        name:'nature',
+        component:() => import('../views/nature/index.vue')
       }
     ]
   }
